215157f4d5444ac8af73993885d36b3335409faa
luisgarduci <112954236+luisgarduci@users.noreply.github.com>
2024-05-02 12:06:52 -0300

    alteração

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ server.clients.forEach((client) => client.send(data.toString()))
 
 //verifica os usuários conectados ao servidor
 
-const server2 = new WebSocketServer({port: 9092})
+/*const server2 = new WebSocketServer({port: 9092})
 
 server2.on("connection", (ws) => {
 ws.on("error", console.error)
@@ -24,5 +24,5 @@ ws.on("message", (data) => {
 server2.clients.forEach((client) => client.send(data.toString()))
 })
 })
-
+*/
 

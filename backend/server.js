9ce8add90aed4ea484e06b146a2965007ce5400f
luisgarduci <112954236+luisgarduci@users.noreply.github.com>
2025-03-24 17:39:34 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const {WebSocketServer} = require('ws')
 
-onst port = process.env.PORT || 10000;
+const port = process.env.PORT || 10000;
 
 const server = new WebSocketServer({
   port: port,
-  host: 'https://chat-online-2s9e.onrender.com', // Garante que o servidor aceita conexões externas
+  host: '0.0.0.0', // Garante que o servidor aceita conexões externas
 });
 
 //servidor para mandar e receber mensagens
